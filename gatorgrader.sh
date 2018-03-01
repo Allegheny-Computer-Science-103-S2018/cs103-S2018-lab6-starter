783adfebf8c58a7b9b9d0e94bceb6e317cb5ef6a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-03-01 10:48:18 -0500

    Updating the gatorgrader script to check for correct commit count.

--- gatorgrader.sh
+++ gatorgrader.sh
@@ -219,7 +219,7 @@ if [ "$CHECK" = true ]; then
   python3 gatorgrader/gatorgrader.py --nowelcome --directories writing --checkfiles reflection.md --paragraphs 5
   determine_exit_code $?
   # --> GatorGrader CHECK: the repository contains at least five commits (update to current + 5)
-  python3 gatorgrader/gatorgrader.py --nowelcome --commits 5
+  python3 gatorgrader/gatorgrader.py --nowelcome --commits 20
   determine_exit_code $?
   echo ""
   printf "%s\n" "${blu}... Finished checking with GatorGrader${end}"
